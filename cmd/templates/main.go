15f6d5428d6763ac0984383af08e145ee2fe1482
jxlxx <wormperson69@gmail.com>
2023-10-08 11:47:10 -0400

    :sparkles: added exchange to template generator

--- cmd/templates/main.go
+++ cmd/templates/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/jxlxx/GreenIsland/world"
+import (
+	"github.com/jxlxx/GreenIsland/exchange"
+	"github.com/jxlxx/GreenIsland/world"
+)
 
 func main() {
 	world.CreateTemplate(world.Country{}, "templates/country.yaml")
 	world.CreateTemplate(world.Company{}, "templates/company.yaml")
+	world.CreateTemplate(exchange.Exchange{}, "templates/exchange.yaml")
 }
